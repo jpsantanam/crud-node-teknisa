e68c1551acbfb9273f70221d19458df148565746
João Pedro Santana Marques <32452904+jpsantanam@users.noreply.github.com>
2023-06-16 11:31:20 -0300

    Adiciona endpoints restantes
    Adiciona os endpoints de READ, UPDATE e DELETE e refatora funções úteis

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const port = 5000;
 
 app.use(bodyParser.json());
 
-app.get('/syncDatabase', async(req, res) => {
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.get('/syncDatabase', async (req, res) => {
     const database = require('./database/db');
 
     try {
@@ -19,31 +23,79 @@ app.get('/syncDatabase', async(req, res) => {
     }
 });
 
-app.post('/createProgrammer', async(req, res) => {
+app.post('/createProgrammer', async (req, res) => {
     try {
         const params = req.body;
 
         const properties = ['name', 'javascript', 'java', 'python'];
 
-        const check = properties.every((property) => {
-            return property in params;
-        });
-
-        if(!check){
-            const propStr = properties.join(',');
-        res.send(`All parameters needed to create a programmer must be sent ${propStr}`);
-        return;
-        };
+        validadeProperties(properties, params, 'every');
 
         const newProgrammer = await programmer.create({
             name: params.name,
             javascript: params.javascript,
             java: params.java,
             python: params.python,
-        })
+        });
 
         res.send(newProgrammer);
 
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get('/retrieveProgrammer', async (req, res) => {
+    try {
+        const params = req.body;
+        if ('id' in params) {
+            const record = await programmer.findByPk(params.id);
+            if (record) {
+                res.send(record);
+            } else {
+                res.send('No programmer found using received ID');
+            }
+            return;
+        }
+        const records = await programmer.findAll();
+        res.send(records);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.put('/updateProgrammer', async (req, res) => {
+    try {
+        const params = req.body;
+
+        const record = await validateID(params);
+
+        const properties = ['name', 'javascript', 'java', 'python'];
+
+        validadeProperties(properties, params, 'some');
+
+        record.name = params.name || record.name;
+        record.python = params.python || record.python;
+        record.java = params.java || record.java;
+        record.javascript = params.javascript || record.javascript;
+
+        await record.save();
+
+        res.send(`${record.id} ${record.name} - Updated successfully`);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.delete('/deleteProgrammer', async (req, res) => {
+    try {
+        const params = req.body;
+
+        const record = await validateID(params);
+
+        await record.destroy();
+
+        res.send(`${record.id} ${record.name} - Deleted successfully`);
     } catch (err) {
         res.send(err);
     }
@@ -53,10 +105,32 @@ app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 });
 
-const validadeProperties = async(properties, params, fn) => {
+const validadeProperties = async (properties, params, fn) => {
+    try {
+        const check = properties[fn]((property) => {
+            return property in params;
+        });
+        if (!check) {
+            const propStr = properties.join(', ');
+            throw `Request body doesn't have any of the following properties: ${propStr}`;
+        }
+        return true;
+    } catch (err) {
+        throw err;
+    }
+};
 
-}
-
-const validateID = async(params) => {
-
-}
+const validateID = async (params) => {
+    try {
+        if (!('id') in params) {
+            throw `Missing 'id' in request body`;
+        }
+        const record = await programmer.findByPk(params.id);
+        if (!record) {
+            throw `Programmer ID not found.`;
+        }
+        return record;
+    } catch (err) {
+        throw err;
+    }
+};
